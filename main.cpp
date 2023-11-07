1ce46cfa8157be2ed86a56b861c7538af2fffbec
SenagaByakko <shogo.m.abiko.1213@gmail.com>
2023-11-07 03:03:25 +0000

    動作プロセスの見直し、中・後脚の昇降システム再構築

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 #include <cstdio>
 
 #define SHOULDER_ACCELERATION 0.3
-#define SHOULDER_BACK_LIMIT 0.7
-#define SHOULDER_DOWN_SPEED 0.6
-#define CENTER_ACCELERATION 0.6
-#define CURTAIN_ACCELERATION 0.8
-#define HELPER_ACCELERATION 0.8
+#define SHOULDER_DOWN_SPEED 0.9
+#define CENTER_ACCELERATION 0.4
+#define CURTAIN_ACCELERATION 0.3
+#define HELPER_ACCELERATION 0.6
 
 SPI SPI_bus(PA_7, PA_6, PA_5);
 DigitalOut SPI_head(PB_8);
@@ -16,14 +15,14 @@ DigitalOut SPI_head(PB_8);
 RawSerial PC(USBTX, USBRX, 115200);
 RawSerial XBee(PC_10, PC_11, 230400);
 
-DriveMotor M_lifting(PB_1, PB_6, 0.5);
+DriveMotor M_shoulder(PB_1, PB_6, 0.5);
 DriveMotor M_helper(PB_15, PA_10);
-DriveMotor M_curtain(PB_14, PA_9);
+DriveMotor M_hindfoot(PB_14, PA_9);
 DriveMotor M_center(PB_13, PA_8);
 
 PwmOut S_tail(PC_8);
 
-RotaryEncoder RE_lifting(PA_14, PA_13);
+RotaryEncoder RE_shoulder(PA_14, PA_13);
 // RotaryEncoder RE_center(PH_1, PH_0);
 RotaryEncoder RE_center(PC_3, PC_2);
 
@@ -34,13 +33,12 @@ DigitalIn SW_helper_lower(PC_1);
 
 DigitalOut LED_MAIN(PB_7);
 
-int beyondAuto(void);
+void beyondAuto(void);
 void liftShoulder(void);
-void liftShoulderAuto(int flag);
 void liftHelper(void);
+void liftCenterAuto(int center_target_height);
 void liftCenter(void);
-void liftCenterManual(void);
-void breakCurtain(void);
+void liftHindfoot(void);
 void moveTail(void);
 inline void receiveSignal(void);
 inline void decordSignal(void);
@@ -61,21 +59,18 @@ int neck_rz_sign = 0x00;
 int check_sum_sign = 0x00;
 int reply_sign = 0x0000;
 
-int helper_flag = 0;
+int advanced_flag = 0;
 int auto_start = 0;
-int auto_start_old = 0;
 int lift_direction = 0;
-int curtain_break = 0;
+int helper_direction = 0;
 int encoder_lock = 0;
 
 int check_sum_correct = 0;
 
-int lifting_target_height = 0;
-int center_target_height = 0;
 double helper_target_speed = 0.0;
-double lifting_target_speed = 0.0;
+double shoulder_target_speed = 0.0;
 double center_target_speed = 0.0;
-double curtain_target_speed = 0.0;
+double hindfoot_target_speed = 0.0;
 
 void initRobot(void) {
     NVIC_SetPriority(ADC_IRQn, 0);
@@ -83,15 +78,15 @@ void initRobot(void) {
     XBee.attach(&receiveSignal, SerialBase::RxIrq);
     SPI_bus.format(16, 1);
     SPI_bus.frequency(20000000);
-    M_lifting.setFrequency(1500);
-    M_lifting.stop();
+    M_shoulder.setFrequency(1500);
+    M_shoulder.stop();
     M_helper.setFrequency(936);
     M_helper.stop();
-    M_curtain.setFrequency(936);
-    M_curtain.stop();
+    M_hindfoot.setFrequency(936);
+    M_hindfoot.stop();
     M_center.setFrequency(936);
     M_center.stop();
-    RE_lifting.Reset();
+    RE_shoulder.Reset();
     RE_center.Reset();
     // RE_helper.Reset();
     SW_shoulder_upper.mode(PullUp);
@@ -102,14 +97,11 @@ void initRobot(void) {
 }
 
 int main() {
-    int shoulder_auto_flag = 0;
-    int signal_sel = 1;
 
     initRobot();
 
     while(true) {
         wait_us(500);
-        shoulder_auto_flag = auto_start;
 
         if(reply_sign == 0x00FF) {
             LED_MAIN = 1;
@@ -119,153 +111,98 @@ int main() {
         }
 
         if(!encoder_lock) {
-            sendSignal(signal_sel);
             moveTail();
         }
+
+        hindfoot_target_speed = 0.0;
+        center_target_speed = 0.0;
+        shoulder_target_speed = 0.0;
+        helper_target_speed = 0.0;
+
+        if(advanced_flag) {
+            hindfoot_target_speed = -lift_direction * 0.7;
+            center_target_speed = -helper_direction;
+        }
         else {
-            sendSignal(0);
+            shoulder_target_speed = lift_direction;
+            helper_target_speed = -helper_direction;
         }
 
-        if(shoulder_auto_flag) {
-            liftShoulderAuto(beyondAuto());
-        }
-        else {
-            lifting_target_speed = lift_direction;     
-            center_target_height = RE_center.Get_Count();  
-            liftShoulder(); 
+        if(auto_start) {
+            beyondAuto();
         }
 
-        if(helper_flag) {
-            if(curtain_break != 0) {
-                center_target_speed = -curtain_break;
-            }
-            else {
-                center_target_speed = 0.0;
-            }
-            liftCenterManual();
-            center_target_height = RE_center.Get_Count();
-        }
-        else {
-            helper_target_speed = -curtain_break;
-            center_target_speed = 0.0;
-            liftCenter();
-        }
+        liftShoulder();
+        liftCenter();
+        liftHindfoot();
         liftHelper();
-        breakCurtain();
 
-        // PC.printf("\033[H");
-        // PC.printf("EN_shoulder:\t%05d\r\nshoulder:\t%05.2lf\r\ncenter: \t%05.2lf\r\nhelper: \t%05.2lf\r\ncurtain:\t%05.2lf",RE_lifting.Get_Count(),M_lifting.read(),M_center.read(),M_helper.read(),M_curtain.read());
+        PC.printf("\033[H");
+        PC.printf("EN_shoulder:\t%05d\r\nshoulder:\t%05.2lf\r\nEN_center:\t%05d\r\ncenter: \t%05.2lf\r\nhelper: \t%05.2lf\r\ncurtain:\t%05.2lf",RE_shoulder.Get_Count(),M_shoulder.read(),RE_center.Get_Count(),M_center.read(),M_helper.read(),M_hindfoot.read());
         // PC.printf("center : %5d\r\n", RE_center.Get_Count());
-        // PC.printf("lifting : %5d\r\n", RE_lifting.Get_Count());
-
-        signal_sel = (signal_sel % 3) + 1;
-        //↑絶対にコメントアウトするな
+        // PC.printf("shoulder : %5d\r\n", RE_shoulder.Get_Count());
 
         // PC.printf("\r\n%d\r\n",signal_sel);
-        // PC.printf("lifting: %lf\r\n", M_lifting.readPWM());
+        // PC.printf("shoulder: %lf\r\n", M_shoulder.readPWM());
     }
 }
 
-int beyondAuto(void) {
+void beyondAuto(void) {
     static int count = 0;
     int flag = 0;
     encoder_lock = 1;
 
     // PC.printf("\033[H%5d", count);
 
-    if(helper_flag == 1) {
+    if(advanced_flag) {
         count = 0;
+    }
+
+    if(count < 100) {
         RE_center.Reset();
     }
-    else {
-        if(count > 0 && count < 100) {
-            RE_center.Reset();
-        }
-        else if(count < 3000) {
-            center_target_height = -270;
-            flag = 0;
-        }
-        else if(count < 6000) {
-            center_target_height = 0;
-            flag = 0;
-        }
-        else if(count < 6500) {
-            lifting_target_speed = 0.0;
-            center_target_height = 950;
-            flag = 1;
-        }
-        else if(count < 12500) { 
-            lifting_target_height = RE_center.Get_Count();
-            center_target_height = 950;
-            flag = 0;
-        }
-        else if(count < 20000) {
-            lifting_target_height = 400;
-            center_target_height = 0;
-            flag = 0;
-        }
-        else if(count < 23500) {
-            lifting_target_height = 0;
-            center_target_height = 0;
-            flag = 0;
-        }
-        else if(count < 25000) {
-            center_target_height = 100;
-        }
-        else if(count > 25500) {
-            count = 0;
-            flag = 0;
-
-        }
-        count++;
+    else if(count < 400) {
+        liftCenterAuto(1000);
     }
-    return flag;
+    else if(count < 800) {
+        liftCenterAuto(0);
+    }
+    else if(count < 900) {
+        hindfoot_target_speed = -0.7;
+    }
+    else if(count < 1100) {
+        hindfoot_target_speed = 0.0;
+    }
+    else if(count < 1200) {
+        hindfoot_target_speed = 0.7;
+    }
+    else if(count < 1300) {
+        hindfoot_target_speed = 0.0;
+    }
+    else if(count > 1300) {
+        count = 0;
+    }
+    count++;
 }
 
 void liftShoulder(void) {
-    double current_speed = M_lifting.read();
-    current_speed += (lifting_target_speed - current_speed) * SHOULDER_ACCELERATION;
+    double current_speed = M_shoulder.read();
+    current_speed += (shoulder_target_speed - current_speed) * SHOULDER_ACCELERATION;
     
-    if(!helper_flag) {
-        if(RE_lifting.Get_Count() > -10 && current_speed < 0) {
-            current_speed = 0;
-        }
+    if(RE_shoulder.Get_Count() > -10 && current_speed < 0) {
+        current_speed = 0;
     }
-
+    
     if(SW_shoulder_upper.read() == 0 && current_speed > 0) {
         current_speed = 0;
     }
     
     if(current_speed < 0) {
-        if(!helper_flag) {
-            current_speed *= SHOULDER_DOWN_SPEED;
-        }
+        current_speed *= SHOULDER_DOWN_SPEED;
     } 
-    M_lifting.drive(current_speed);
+    M_shoulder.drive(current_speed);
 
-    // PC.printf("%d, %lf, %lf\r\n", RE_lifting.Get_Count(), lifting_target_speed, M_lifting.read());
-}
-
-void liftShoulderAuto(int flag) {
-    double current_height = RE_lifting.Get_Count();
-    double target_speed;
-    
-    if(flag) {
-        double current_speed = M_lifting.read();
-        current_speed += (lifting_target_speed - current_speed) * SHOULDER_ACCELERATION;
-        M_lifting.drive(current_speed);
-        // PC.printf("%lf\r\n", current_speed);
-    }
-    else {
-        if(lifting_target_height + 100 < current_height) target_speed = 0.8;
-        else if(lifting_target_height - 100 > current_height) target_speed = -0.8;
-        else target_speed = 0;
-
-        double current_speed = M_lifting.read();
-        current_speed += (target_speed - current_speed) * SHOULDER_ACCELERATION;
-        if(current_speed < 0) current_speed *= SHOULDER_DOWN_SPEED;
-        M_lifting.drive(current_speed); 
-    }
+    // PC.printf("%d, %lf, %lf\r\n", RE_shoulder.Get_Count(), shoulder_target_speed, M_shoulder.read());
 }
 
 void liftHelper(void) {
@@ -283,28 +220,24 @@ void liftHelper(void) {
     // PC.printf("%03.2lf\r\n", M_helper.read());
 }
 
-void liftCenter(void) {
+void liftCenterAuto(int center_target_height) {
     double current_height = RE_center.Get_Count();
-    double current_speed = M_center.read();
     
-    if(center_target_height - 10 > current_height) center_target_speed = 1.0;
-    else if(center_target_height + 10 < current_height) center_target_speed = -1.0;
+    if(center_target_height - 30 > current_height) center_target_speed = -0.8;
+    else if(center_target_height + 30 < current_height) center_target_speed = 0.8;
     else center_target_speed = 0;
-
-    current_speed += (center_target_speed - current_speed) * CENTER_ACCELERATION;
-    M_center.drive(current_speed); 
 }
 
-void liftCenterManual(void) {
+void liftCenter(void) {
     double current_speed = M_center.read();
     current_speed += (center_target_speed - current_speed) * CENTER_ACCELERATION;
     M_center.drive(current_speed);
 }
 
-void breakCurtain(void) {
-    double current_speed = M_curtain.read();
-    current_speed += (curtain_target_speed - current_speed) * CURTAIN_ACCELERATION;
-    M_curtain.drive(current_speed);
+void liftHindfoot(void) {
+    double current_speed = M_hindfoot.read();
+    current_speed += (hindfoot_target_speed - current_speed) * CURTAIN_ACCELERATION;
+    M_hindfoot.drive(current_speed);
 }
 
 void moveTail(void) {
@@ -356,25 +289,23 @@ inline void receiveSignal(void) {
 }
 
 inline void decordSignal(void) {
-    auto_start_old = auto_start;
-
-    helper_flag = (extended_sign >> 7) % 0b10;
+    advanced_flag = (extended_sign >> 7) % 0b10;
     auto_start = (extended_sign >> 6) % 0b10;
     lift_direction = (extended_sign >> 4) % 0b100;
-    curtain_break = (extended_sign >> 2) % 0b100;
+    helper_direction = (extended_sign >> 2) % 0b100;
     encoder_lock = (extended_sign >> 1) % 0b10;
 
     if(lift_direction == 2) lift_direction = 1;
     else if(lift_direction == 1) lift_direction = -1;
     else lift_direction = 0;
     
-    if(curtain_break == 2) curtain_break = 1;
-    else if(curtain_break == 1) curtain_break = -1;
-    else curtain_break = 0;
+    if(helper_direction == 2) helper_direction = 1;
+    else if(helper_direction == 1) helper_direction = -1;
+    else helper_direction = 0;
 
     // PC.printf("%#02X\r\n", extended_sign);
-    PC.printf("\033[H0x%02X%02X%02X%02X, %d", extended_sign, chin_sign, neck_rx_sign, neck_ry_sign, SW_helper_lower.read());
-    // PC.printf("%d, %d, %d, %d, %d\r\n", helper_flag, auto_start, lift_direction, curtain_break, encoder_lock);
+    // PC.printf("\033[H0x%02X%02X%02X%02X", extended_sign, chin_sign, neck_rx_sign, neck_ry_sign);
+    // PC.printf("%d, %d, %d, %d, %d\r\n", advanced_flag, auto_start, lift_direction, helper_direction, encoder_lock);
 }
 
 inline void sendSignal(int format) {
